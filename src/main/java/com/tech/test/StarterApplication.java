19c60f433d0b8b6085eafcad9b66716554c8025e
JamshidUK <jashsha20@gmail.com>
2019-11-10 21:14:58 +0100

    standard calculator

---
+++ src/main/java/com/tech/test/StarterApplication.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.tech.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application main class.
+ * 
+ * @author Jamshid
+ */
+@SpringBootApplication
+public class StarterApplication {
+
+	/**
+	 * Main class.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(StarterApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/tech/test/service/StandardCalculatorController.java
@@ -0,0 +1,85 @@
+/**
+ * 
+ */
+package com.tech.test.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+/**
+ * Standard calculator controller.
+ * 
+ * @author Jamshid
+ */
+@RestController
+public class StandardCalculatorController {
+	
+	@RequestMapping("/add")
+    public double add(@RequestParam(value="a") String a, @RequestParam(value="b") String b) {
+		double sum = 0;
+		try{
+			double input1 = Double.parseDouble(a);
+			double input2 = Double.parseDouble(b);
+			sum = input1 + input2;
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "Invalid Input", e);
+		}
+		
+		return sum;
+    }
+	
+	@RequestMapping("/sub")
+    public double sub(@RequestParam(value="a") String a, @RequestParam(value="b") String b) {
+		double diff = 0;
+		try{
+			double input1 = Double.parseDouble(a);
+			double input2 = Double.parseDouble(b);
+			diff = input1 - input2;
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "Invalid Input", e);
+		}
+		
+		return diff;
+    }
+	
+	@RequestMapping("/mul")
+    public double mul(@RequestParam(value="a") String a, @RequestParam(value="b") String b) {
+		double prod = 0;
+		try{
+			double input1 = Double.parseDouble(a);
+			double input2 = Double.parseDouble(b);
+			prod = input1 * input2;
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "Invalid Input", e);
+		}
+		
+		return prod;
+    }
+	
+	@RequestMapping("/div")
+    public double div(@RequestParam(value="a") String a, @RequestParam(value="b") String b) {
+		double division = 0;
+		boolean invalidDenominator = false;
+		try{
+			double input1 = Double.parseDouble(a);
+			double input2 = Double.parseDouble(b);
+			if (input2 != 0.0) {
+				division = input1 / input2;
+			} else {
+				invalidDenominator = true;
+			}
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "Invalid Input", e);
+		}
+		
+		if (invalidDenominator) {
+			throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "Invalid Denominator");
+		}
+		
+		return division;
+    }
+
+}
